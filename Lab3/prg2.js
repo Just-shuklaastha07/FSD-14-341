1c52e9454b3ccb5172aa915f35eacc5314feaf15
Just-shuklaastha07 <shuklaastha07@gmail.com>
2026-08-20 14:35:11 +0530

    Program 1&2

---
+++ Lab3/prg2.js
@@ -0,0 +1,12 @@
+import http from "http";
+const server=http.createServer((req,res)=>{
+    //res.writeHead(404,{
+     //   "Content-type": "text/plain"
+   // });
+    res.end("<h1>Welcome to server</h1>");
+});
+
+const PORT = 5000;
+server.listen(PORT,()=>{
+    console.log("Server is running on http://localhost:"+PORT);
+});
